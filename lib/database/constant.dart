2781a7155b2ed417878111e05dc85858c1e49803
Aron <aronbautista61@gmail.com>
2026-05-07 22:52:37 +0800

    constant

--- lib/database/constant.dart
+++ lib/database/constant.dart
@@ -1,2 +1,2 @@
-const mongoUri = "mongodb+srv://aronbautista61_db_user:vyVrI9TzOTAJwKF6@reodesu.0hfltxh.mongodb.net/?appName=reodesu";
-const collectionName = "mobile_users";
+const mongoUri = "";
+const collectionName = "";
